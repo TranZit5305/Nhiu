434003b95af3e734d70c7654c8fd2bfed3783000
KidoKosho <152004626+KidoKosho@users.noreply.github.com>
2025-07-28 23:33:09 +0700

    kdio

--- js/base.js
+++ js/base.js
@@ -1,49 +1,60 @@
-const text = "Chào bạn, đây là hiệu ứng hiện từng chữ!";
-const container = document.querySelector(".text-container");
-const video = document.getElementById("myVideo");
-const replayBtn = document.getElementById("replayBtn");
-const pageContent = document.querySelector(".page-content");
-const bgMusic = document.getElementById("bg-music");
+  const text = "Chào bạn, đây là hiệu ứng hiện từng chữ!";
+  const container = document.querySelector(".text-container");
+  const video = document.getElementById("myVideo");
+  const replayBtn = document.getElementById("replayBtn");
+  const pageContent = document.querySelector(".page-content");
+  const bgMusic = document.getElementById("bg-music");
 
-let index = 0;
-
-// Gõ từng chữ
-function typeLetter() {
-  if (index < text.length) {
-    container.textContent += text.charAt(index++);
-    setTimeout(typeLetter, 80);
+  let index = 0;
+  // JavaScript
+  function showCustomAlert() {
+    document.getElementById('customAlert').style.display = 'block';
   }
-}
-function loinoidau(){
-  alert("Cảm ơn bạn Chút đã vào đây,cảm ơn bạn rất nhiều ,mong bạn sẽ xem hết video nhé :>");
-}
-// Xử lý video
-video.addEventListener("play", () => bgMusic.pause());
-video.addEventListener("ended", () => {
-  alert("Cảm ơn bạn đã xem hết video!");
-  replayBtn.hidden = false;
-  bgMusic.play();
-});
 
-replayBtn.addEventListener("click", () => {
-  video.currentTime = 0;
-  video.play();
-  replayBtn.hidden = true;
-});
-
-// Ngăn chuột phải
-// document.addEventListener("contextmenu", e => e.preventDefault());
-
-document.addEventListener("DOMContentLoaded", () => {
-  if (sessionStorage.getItem("isLoggedIn") !== "true") {
-    alert("Bạn chưa nhập mật khẩu vui lòng nhập mật khẩu....")
-    window.location.href = "../html/login.html"; // Đổi đúng tên file login của bạn
-    return;
+  function closeAlert() {
+    document.getElementById('customAlert').style.display = 'none';
   }
-  document.body.style.display = "block"
-  pageContent.hidden = false;// Hiện trang
-  // loinoidau();
-  typeLetter();
-  initSnow();
-  animateSnow();
-});
+  // Gõ từng chữ
+  function typeLetter() {
+    if (index < text.length) {
+      container.textContent += text.charAt(index++);
+      setTimeout(typeLetter, 80);
+    }
+  }
+  function loinoidau(){
+    alert("Cảm ơn bạn Chút đã vào đây,cảm ơn bạn rất nhiều ,mong bạn sẽ xem hết video nhé :>");
+  }
+  // Xử lý video
+  video.addEventListener("play", () => bgMusic.pause());
+  video.addEventListener("ended", () => {
+    alert("Cảm ơn bạn đã xem hết video!");
+    replayBtn.hidden = false;
+    bgMusic.play();
+  });
+
+  replayBtn.addEventListener("click", () => {
+    video.currentTime = 0;
+    video.play();
+    replayBtn.hidden = true;
+  });
+
+  // Ngăn chuột phải
+  // document.addEventListener("contextmenu", e => e.preventDefault());
+
+  document.addEventListener("DOMContentLoaded", () => {
+    if (sessionStorage.getItem("isLoggedIn") !== "true") {
+      alert("Bạn chưa nhập mật khẩu vui lòng nhập mật khẩu....")
+      window.location.href = "../html/login.html"; // Đổi đúng tên file login của bạn
+      return;
+    }
+    document.body.style.display = "block"
+    pageContent.hidden = false;// Hiện trang
+    
+    // loinoidau();
+    window.onload = () => {
+      setTimeout(showCustomAlert, 1000);
+    };
+    typeLetter();
+    initSnow();
+    animateSnow();
+  });
